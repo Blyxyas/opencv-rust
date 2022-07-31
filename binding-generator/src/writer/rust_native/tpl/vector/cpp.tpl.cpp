7cc717dd18253b382766b940a37bfe7faaf3364a
Alex <blyxyas@gmail.com>
2022-07-31 13:20:40 +0200

    typo fixed in`binding-generator/src/writer/.../cpp.tpl.cpp`

--- binding-generator/src/writer/rust_native/tpl/vector/cpp.tpl.cpp
+++ binding-generator/src/writer/rust_native/tpl/vector/cpp.tpl.cpp
@@ -19,7 +19,7 @@ extern "C" {
 		return instance->capacity();
 	}
 
-	void cv_{{rust_localias}}_resize({{cpp_full}}* instance, size_t new_size) {
+	void cv_{{rust_localalias}}_resize({{cpp_full}}* instance, size_t new_size) {
 		instance->resize(new_size)
 	}
 
